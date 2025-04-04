ce9b91fb548b8488bb81c0f9bb9d3825a1bc07af
Masha Karhapoltseva <23MKarhapoltseva@heckgrammar.co.uk>
2025-04-04 10:15:41 +0100

    finished

--- BlankCSDotNet8/Program.cs
+++ BlankCSDotNet8/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            
+            DateTime current = DateTime.Now;
+            DateTime target = new DateTime(2025, 4, 4, 15, 1, 5);
+          
+            TimeSpan difference = target - current;
+
+            Thread.Sleep(1000);
+            Console.WriteLine(difference + "left till half term holiday");
+            if (Convert.ToInt32 (difference) <= 0 )
+            {
+                Console.WriteLine("The half term holiday has already started");
+            }
+            else
+            {
+                Console.WriteLine(difference.Days + "Left till the half term holiday(days)");
+            }
         }
     }
 }
